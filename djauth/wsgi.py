ed52fade4e439b6ee9c13cbd257935e53544711f
Eric Lin <eric_lin@college.harvard.edu>
2019-03-10 17:41:33 -0400

    starting over

--- djauth/wsgi.py
+++ djauth/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for staff_dashboard project.
+WSGI config for djauth project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'staff_dashboard.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djauth.settings')
 
 application = get_wsgi_application()
--- users/models.py
+++ users/models.py
@@ -1,11 +1,9 @@
 # users/models.py
-
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
 class CustomUser(AbstractUser):
-	# add additional fields here
+    # add additional fields in here
 
-	def __str__(self):
-		return self.email
+    def __str__(self):
+        return self.email
