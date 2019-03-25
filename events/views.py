10acdbfb6084472d97063934cb8bc1672707337f
Eric Lin <eric_lin@college.harvard.edu>
2019-03-25 17:03:14 -0400

    connecting html with views

--- events/views.py
+++ events/views.py
@@ -5,6 +5,10 @@ from .forms import EventForm
 def index(request):
 	if request.method == "GET":
 		print("GET request")
+		events = Event.objects.values()	# get querySet of events
+		print(events)
+		context = {'events': events} 	
+		return render(request, 'events.html',context)		# render events html
 
 	elif request.method == "POST":
 		print("POST request")
