8ccce6634b2d5800582b5584d487e5fe0c368326
irobinson1637 <irobinson1637@gmail.com>
2019-03-27 02:41:08 -0400

    formatted code

--- static/js/script.js
+++ static/js/script.js
@@ -1,173 +1,128 @@
-  var margin = {top: 10, right: 10, bottom: 10, left: 10}
-  , width = window.innerWidth - margin.left - margin.right // Use the window's width 
-  , height = window.innerHeight - margin.top - margin.bottom; // Use the window's height
-
-/*
-// The number of datapoints
-var n = 21;
-
-// 5. X scale will use the index of our data
-var xScale = d3.scaleLinear()
-    .domain([0, n-1]) // input
-    .range([0, width/2]); // output
-
-// 6. Y scale will use the randomly generate number 
-var yScale = d3.scaleLinear()
-    .domain([0, 1]) // input 
-    .range([height/2, 0]); // output 
-
-// 7. d3's line generator
-var line = d3.line()
-    .x(function(d, i) { return xScale(i); }) // set the x values for the line generator
-    .y(function(d) { return yScale(d.y); }) // set the y values for the line generator 
-    .curve(d3.curveMonotoneX) // apply smoothing to the line
-
-// 8. An array of objects of length N. Each object has key -> value pair, the key being "y" and the value is a random number
-var dataset = d3.range(n).map(function(d) { return {"y": d3.randomUniform(1)() } })
-
-// 1. Add the SVG to the page and employ #2
-var svg = d3.select(".hero-body").append("svg")
-    .attr("width", width/2.5 + margin.left + margin.right)
-    .attr("height", height/2.5 + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform", "translate(" + margin.left + "," + 0+ ")");
-
-// 3. Call the x axis in a group tag
-svg.append("g")
-    .attr("class", "x axis")
-    .attr("transform", "translate(0," + height/2.5 + ")")
-    .call(d3.axisBottom(xScale)); // Create an axis component with d3.axisBottom
-
-// 4. Call the y axis in a group tag
-svg.append("g")
-    .attr("class", "y axis")
-    .call(d3.axisLeft(yScale)); // Create an axis component with d3.axisLeft
-
-// 9. Append the path, bind the data, and call the line generator 
-svg.append("path")
-    .datum(dataset) // 10. Binds data to the line 
-    .attr("class", "line") // Assign a class for styling 
-    .attr("d", line); // 11. Calls the line generator 
-
-// 12. Appends a circle for each datapoint 
-svg.selectAll(".dot")
-    .data(dataset)
-  .enter().append("circle") // Uses the enter().append() method
-    .attr("class", "dot") // Assign a class for styling
-    .attr("cx", function(d, i) { return xScale(i) })
-    .attr("cy", function(d) { return yScale(d.y) })
-    .attr("r", 5)
-      .on("mouseover", function(a, b, c) { 
-  			console.log(a) 
-        this.attr('class', 'focus')
-		})
-      .on("mouseout", function() {  })  */
+var margin = { top: 10, right: 10, bottom: 10, left: 10 },
+  width = window.innerWidth - margin.left - margin.right, // Use the window's width
+  height = window.innerHeight - margin.top - margin.bottom; // Use the window's height
 
 //MAP
-  var active = d3.select(null);
-        var svg = d3.select(".hero-body").append("svg")
-            .attr('class', 'center-container')
-            .attr('width', width + margin.left + margin.right)
-            .attr('height', height + margin.top + margin.bottom);
+var active = d3.select(null);
+var svg = d3
+  .select(".hero-body")
+  .append("svg")
+  .attr("class", "center-container")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom);
 
-        svg.append('rect')
-            .attr('class', 'background-center-container')
-            .attr('width', width + margin.left + margin.right)
-            .attr('height', height + margin.top + margin.bottom)
-            .on('click', clicked);
+svg
+  .append("rect")
+  .attr("class", "background-center-container")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .on("click", clicked);
 
-    //var clickStore;
-    //var scaleStore;
-   d3.json('https://d3js.org/us-10m.v1.json')
-            .then(ready).catch(function(error) {
-              console.log(error);
-            })
-      
-        var projection = d3.geoAlbers()
-            .translate([width/2.5 , height/2.5])
-            .scale(width/5);
+d3.json("https://d3js.org/us-10m.v1.json")
+  .then(ready)
+  .catch(function(error) {
+    console.log(error);
+  });
 
-        var path = d3.geoPath()
-           // .projection(projection);
+var projection = d3
+  .geoAlbers()
+  .translate([width / 2.5, height / 2.5])
+  .scale(width / 5);
 
-        var g = svg.append("g")
-            .attr('class', 'center-container center-items us-state')
-            //.attr('transform', 'translate('+margin.left+','+margin.top+')')
-            .attr('width', width + margin.left + margin.right)
-            .attr('height', height + margin.top + margin.bottom);
+var path = d3.geoPath();
+// .projection(projection);
 
-        function ready(us) {
+var g = svg
+  .append("g")
+  .attr("class", "center-container center-items us-state")
+  //.attr('transform', 'translate('+margin.left+','+margin.top+')')
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom);
 
-            g.append("g")
-                .attr("id", "counties")
-                .selectAll("path")
-                .data(topojson.feature(us, us.objects.counties).features)
-                .enter().append("path")
-                .attr("d", path)
-                .attr("class", "county-boundary")
-                .on("click", reset);
+function ready(us) {
+  g.append("g")
+    .attr("id", "counties")
+    .selectAll("path")
+    .data(topojson.feature(us, us.objects.counties).features)
+    .enter()
+    .append("path")
+    .attr("d", path)
+    .attr("class", "county-boundary")
+    .on("click", reset);
 
-             g.append("g")
-                .attr("id", "states")
-                .selectAll("path")
-                .data(topojson.feature(us, us.objects.states).features)
-                .enter().append("path")
-                .attr("d", path)
-                .attr("class", "state")
-                .on("click", clicked); 
-                
-            g.append("path")
-                .datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
-                .attr("id", "state-borders")
-                .attr("d", path);
+  g.append("g")
+    .attr("id", "states")
+    .selectAll("path")
+    .data(topojson.feature(us, us.objects.states).features)
+    .enter()
+    .append("path")
+    .attr("d", path)
+    .attr("class", "state")
+    .on("click", clicked);
 
-                var marks = [{long: 120, lat: 90},{long: 160, lat: 115},{long: 200, lat: 150}];
+  g.append("path")
+    .datum(
+      topojson.mesh(us, us.objects.states, function(a, b) {
+        return a !== b;
+      })
+    )
+    .attr("id", "state-borders")
+    .attr("d", path);
 
-                svg.selectAll(".mark")
-                    .data(marks)
-                    .enter()
-                    .append("image")
-                    .attr('class','mark')
-                    .attr('width', 20)
-                    .attr('height', 20)
-                    .attr("xlink:href",'https://cdn3.iconfinder.com/data/icons/softwaredemo/PNG/24x24/DrawingPin1_Blue.png')
-                    .attr("transform", d => 'translate('+ d.long+','+d.lat+')');
+  var marks = [
+    { long: 120, lat: 90 },
+    { long: 160, lat: 115 },
+    { long: 200, lat: 150 }
+  ];
 
-        }
+  svg
+    .selectAll(".mark")
+    .data(marks)
+    .enter()
+    .append("image")
+    .attr("class", "mark")
+    .attr("width", 20)
+    .attr("height", 20)
+    .attr(
+      "xlink:href",
+      "https://cdn3.iconfinder.com/data/icons/softwaredemo/PNG/24x24/DrawingPin1_Blue.png"
+    )
+    .attr("transform", d => "translate(" + d.long + "," + d.lat + ")");
+}
 
-        function clicked(d) {
-          if (d3.select('.background').node() === this) return reset();
+function clicked(d) {
+  if (d3.select(".background").node() === this) return reset();
 
-          if (active.node() === this) return reset();
+  if (active.node() === this) return reset();
 
-          active.classed("active", false);
-          active = d3.select(this).classed("active", true);
+  active.classed("active", false);
+  active = d3.select(this).classed("active", true);
 
-          var bounds = path.bounds(d),
-              dx = bounds[1][0] - bounds[0][0],
-              dy = bounds[1][1] - bounds[0][1],
-              x = (bounds[0][0] + bounds[1][0]) / 2,
-              y = (bounds[0][1] + bounds[1][1]) / 2,
-              scale = .9 / Math.max(dx / (width), dy / (height)),
-              translate = [(width) / 2 - scale * x, (height) / 2 - scale * y];
+  var bounds = path.bounds(d),
+    dx = bounds[1][0] - bounds[0][0],
+    dy = bounds[1][1] - bounds[0][1],
+    x = (bounds[0][0] + bounds[1][0]) / 2,
+    y = (bounds[0][1] + bounds[1][1]) / 2,
+    scale = 0.9 / Math.max(dx / width, dy / height),
+    translate = [width / 2 - scale * x, height / 2 - scale * y];
 
-          g//.transition()
-             // .delay(100)
-              //.duration(750)
-              .style("stroke-width", 1.5 / scale + "px")
-              .attr("transform", "translate(" + translate + ")scale(" + scale + ")");
-          
-        svg.selectAll(".mark").attr("transform", "translate(" + translate + ")scale(" + scale + ")")
-      }
-        function reset() {
-            active.classed("active", false);
-            d3.active = d3.select(null);
+  g //.transition()
+    // .delay(100)
+    //.duration(750)
+    .style("stroke-width", 1.5 / scale + "px")
+    .attr("transform", "translate(" + translate + ")scale(" + scale + ")");
 
-            g//.transition()
-              //  .delay(100)
-              //  .duration(1000)
-                .style("stroke-width", "1.5px")
-                .attr('transform', 'translate('+margin.left+','+margin.top+')');
+  svg
+    .selectAll(".mark")
+    .attr("transform", "translate(" + translate + ")scale(" + scale + ")");
+}
+function reset() {
+  active.classed("active", false);
+  d3.active = d3.select(null);
 
-        } 
-
+  g //.transition()
+    //  .delay(100)
+    //  .duration(1000)
+    .style("stroke-width", "1.5px")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+}
