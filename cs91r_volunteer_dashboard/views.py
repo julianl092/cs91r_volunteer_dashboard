920c2af4c3dd5fb5e673a93eb2ad947d61a60bf0
Eric Lin <eric_lin@college.harvard.edu>
2019-05-05 17:51:13 -0400

    linking volunteer and event locations

--- cs91r_volunteer_dashboard/views.py
+++ cs91r_volunteer_dashboard/views.py
@@ -2,23 +2,48 @@ from django.shortcuts import render
 from volunteers.models import Volunteer
 from events.models import Event
 from geopy.geocoders import Nominatim
-import csv
+import csv, json
+import string
+from geojson import Feature, FeatureCollection, Point
 
 def home(request): 
 	vols = Volunteer.objects.all()
 	# Find latitude, longitude for each address
-	coords = []
+	features = []
 	for volunteer in vols.values():
 		name = volunteer['name']
 		address = volunteer['address']
 		geolocator = Nominatim()
 		location = geolocator.geocode(address)
-		coords.append([location.latitude, location.longitude, name])
-	# Redo csv file for geo addresses
-	filename = "static/js/airport.csv"
-	with open("static/js/airport.csv", "w+") as writeFile:
-		writer = csv.writer(writeFile)
-		writer.writerows(coords)
+		latitude, longitude = map(float, (location.latitude, location.longitude))
+		features.append(
+			Feature(
+				geometry = Point((longitude, latitude)),
+				properties = {
+					'name': name,
+				}
+			)
+		)
 
 	evs = Event.objects.all()
+	for event in evs.values():
+		name = event['name']
+		address = event['address']
+		geolocator = Nominatim()
+		location = geolocator.geocode(address)
+		if location:
+			latitude, longitude = map(float, (location.latitude, location.longitude))
+			features.append(
+				Feature(
+					geometry = Point((longitude, latitude)),
+					properties = {
+						'name': name,
+					}
+				)
+			)
+
+	# Write to json
+	collection = FeatureCollection(features)
+	with open("static/js/data.geojson", "w+") as f:
+		f.write('%s' % collection)
 	return render (request, 'home.html', context = {"volunteers": vols[:4], "events": evs[:4]})
